27cafa65d66566d0b1099dae4a777c8ac6d6e851
mburgass <m.burgass14@imperial.ac.uk>
2016-04-19 09:51:26 +0100

    Complete Troms jobs code

--- Livelihoods/Employment_Figures/Norway_Mainland/Norway livelihood code.R
+++ Livelihoods/Employment_Figures/Norway_Mainland/Norway livelihood code.R
@@ -10,6 +10,15 @@ Troms_Travel = Troms_Employment2 %>% filter(Job.Type == "79 Travel agency, tour 
 ## Ocean based employment considering food products likely to be seafood related, travel tourism likely be ocean related
 # Join together
 Troms_Group = rbind(Troms_Fish, Troms_Food, Troms_Water, Troms_Travel)
+Troms_Group2 = select(Troms_Group, -Job.Type)
 Troms_Group2 = gather(Troms_Group, "Year", "Marine.Jobs", 3:8)
 Troms_Group3 = select(Troms_Group2, -Job.Type)
 Troms_Group3
+Troms_Final = Troms_Group3 %>% separate(Year,c("X","Year"),remove=T,sep="X")%>%  #(1) First strip away the X from the years (this creates a new column called "X" that is empty)
+  select(Region,Year,Marine.Jobs)%>%
+  group_by(Year)%>%
+  dplyr::summarize(total_jobs = sum(Marine.Jobs, na.rm=T))%>%
+  ungroup()%>%
+Troms_Final$Region <- "Troms"
+Troms_Final[c(3,1,2)]
+Troms_Final
