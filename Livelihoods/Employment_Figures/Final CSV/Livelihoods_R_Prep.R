d7128994119e98e48ee0033a432913e7a3d33fba
mburgass <m.burgass14@imperial.ac.uk>
2016-04-20 12:22:14 +0100

    Alaska update

--- Livelihoods/Employment_Figures/Final CSV/Livelihoods_R_Prep.R
+++ Livelihoods/Employment_Figures/Final CSV/Livelihoods_R_Prep.R
@@ -196,3 +196,123 @@ Greenland_jobs_final
 ##Check how best to separate out between east and west Greenland? Roughly 80% of people live in West Greenland
 
 write.csv(Greenland_jobs_final, "Greenland_Jobs.csv")
+
+# USA/Alaska --------------------------------------------------------------
+
+
+# 2014 --------------------------------------------------------------------
+
+Alaska_Employment14 = read_excel("Livelihoods/Employment_Figures/USA/Alaska_Employment_2002_2014.xlsx", sheet = 3, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+Alaska_Employment14<-data.frame(Alaska_Employment14)
+Alaska_Employment14 = dplyr::filter(Alaska_Employment, (AREA.CODE %in% c("000185", "000188")))
+Alaska_Employment14 <- dplyr::tbl_df(Alaska_Employment14)
+Alaska_Employment14 <- dplyr::select(Alaska_Employment14, AREANAME, NAICS.DESCRIPTION, NA., NA..1, NA..2, AVERAGE.EMPLOYMENT)
+Alaska_Employment14 <- dplyr::filter(Alaska_Employment14, AVERAGE.EMPLOYMENT > 0)
+Alaska_Employment14 <- dplyr::filter(Alaska_Employment14, (NA. %in% c("NATURAL RESOURCES AND MINING", "MANUFACTURING", "TRADE, TRANSPORTATION AND UTILITIES", "LEISURE AND HOSPITALITY" )))
+Alaska_Employment14
+
+##Not really any marine jobs listed in these areas. National figures don't seem to be matched by the regional figures though..?
+
+# 2013 --------------------------------------------------------------------
+Alaska_Employment13 = read_excel("Livelihoods/Employment_Figures/USA/Alaska_Employment_2002_2014.xlsx", sheet = 4, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+Alaska_Employment13<-data.frame(Alaska_Employment13)
+Alaska_Employment13 = dplyr::filter(Alaska_Employment13, (AREA.CODE %in% c("000185", "000188")))
+Alaska_Employment13 <- dplyr::tbl_df(Alaska_Employment13)
+Alaska_Employment13 <- dplyr::select(Alaska_Employment13, AREANAME, NAICS.DESCRIPTION, NA., NA..1, NA..2, AVERAGE.EMPLOYMENT)
+Alaska_Employment13 <- dplyr::filter(Alaska_Employment13, AVERAGE.EMPLOYMENT > 0)
+Alaska_Employment13 <- dplyr::filter(Alaska_Employment13, (NA. %in% c("NATURAL RESOURCES AND MINING", "MANUFACTURING", "TRADE, TRANSPORTATION AND UTILITIES", "LEISURE AND HOSPITALITY" )))
+Alaska_Employment13
+
+
+# 2012 --------------------------------------------------------------------
+Alaska_Employment12 = read_excel("Livelihoods/Employment_Figures/USA/Alaska_Employment_2002_2014.xlsx", sheet = 5, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+Alaska_Employment12<-data.frame(Alaska_Employment12)
+Alaska_Employment12 = dplyr::filter(Alaska_Employment12, (AREA.CODE %in% c("000185", "000188")))
+Alaska_Employment12 <- dplyr::tbl_df(Alaska_Employment12)
+Alaska_Employment12 <- dplyr::select(Alaska_Employment12, AREANAME, NAICS.DESCRIPTION, NA., NA..1, NA..2, AVERAGE.EMPLOYMENT)
+Alaska_Employment12 <- dplyr::filter(Alaska_Employment12, AVERAGE.EMPLOYMENT > 0)
+Alaska_Employment12 <- dplyr::filter(Alaska_Employment12, (NA. %in% c("NATURAL RESOURCES AND MINING", "MANUFACTURING", "TRADE, TRANSPORTATION AND UTILITIES", "LEISURE AND HOSPITALITY" )))
+Alaska_Employment12
+
+# 2011 --------------------------------------------------------------------
+Alaska_Employment11 = read_excel("Livelihoods/Employment_Figures/USA/Alaska_Employment_2002_2014.xlsx", sheet = 6, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+Alaska_Employment11<-data.frame(Alaska_Employment11)
+Alaska_Employment11 = dplyr::filter(Alaska_Employment11, (AREA.CODE %in% c("000185", "000188")))
+Alaska_Employment11 <- dplyr::tbl_df(Alaska_Employment11)
+Alaska_Employment11 <- dplyr::select(Alaska_Employment11, AREANAME, NAICS.DESCRIPTION, NA., NA..1, NA..2, AVERAGE.EMPLOYMENT)
+Alaska_Employment11 <- dplyr::filter(Alaska_Employment11, AVERAGE.EMPLOYMENT > 0)
+Alaska_Employment11 <- dplyr::filter(Alaska_Employment11, (NA. %in% c("NATURAL RESOURCES & MINING", "MANUFACTURING", "TRADE, TRANS. & UTILITIES", "LEISURE & HOSPITALITY" )))
+##Note slight difference in Job types i.e. use of & rather than and. To correct later
+Alaska_Employment11
+
+# 2010 --------------------------------------------------------------------
+Alaska_Employment10 = read_excel("Livelihoods/Employment_Figures/USA/Alaska_Employment_2002_2014.xlsx", sheet = 7, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+Alaska_Employment10<-data.frame(Alaska_Employment10)
+Alaska_Employment10 = dplyr::filter(Alaska_Employment10, (AREA.CODE %in% c("000185", "000188")))
+Alaska_Employment10 <- dplyr::tbl_df(Alaska_Employment10)
+Alaska_Employment10 <- dplyr::select(Alaska_Employment10, AREANAME, NAICS.DESCRIPTION, NA., NA..1, NA..2, AVERAGE.EMPLOYMENT)
+Alaska_Employment10 <- dplyr::filter(Alaska_Employment10, AVERAGE.EMPLOYMENT > 0)
+Alaska_Employment10 <- dplyr::filter(Alaska_Employment10, (NA. %in% c("NATURAL RESOURCES & MINING", "MANUFACTURING", "TRADE, TRANS. & UTILITIES", "LEISURE & HOSPITALITY" )))
+Alaska_Employment10
+
+# 2009 --------------------------------------------------------------------
+Alaska_Employment09 = read_excel("Livelihoods/Employment_Figures/USA/Alaska_Employment_2002_2014.xlsx", sheet = 8, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+Alaska_Employment09<-data.frame(Alaska_Employment09)
+Alaska_Employment09 = dplyr::filter(Alaska_Employment09, (AREA.CODE %in% c("000185", "000188")))
+Alaska_Employment09 <- dplyr::tbl_df(Alaska_Employment09)
+Alaska_Employment09 <- dplyr::select(Alaska_Employment09, AREANAME, NAICS.DESCRIPTION, NA., NA..1, NA..2, AVERAGE.EMPLOYMENT)
+Alaska_Employment09 <- dplyr::filter(Alaska_Employment09, AVERAGE.EMPLOYMENT > 0)
+Alaska_Employment09 <- dplyr::filter(Alaska_Employment09, (NA. %in% c("NATURAL RESOURCES & MINING", "MANUFACTURING", "TRADE, TRANS. & UTILITIES", "LEISURE & HOSPITALITY" )))
+Alaska_Employment09
+
+# 2008 --------------------------------------------------------------------
+Alaska_Employment08 = read_excel("Livelihoods/Employment_Figures/USA/Alaska_Employment_2002_2014.xlsx", sheet = 9, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+Alaska_Employment08<-data.frame(Alaska_Employment08)
+Alaska_Employment08 = dplyr::filter(Alaska_Employment08, (AREA.CODE %in% c("000185", "000188")))
+Alaska_Employment08 <- dplyr::tbl_df(Alaska_Employment08)
+Alaska_Employment08 <- dplyr::select(Alaska_Employment08, AREANAME, NAICS.DESCRIPTION, NA., NA..1, NA..2, AVERAGE.EMPLOYMENT)
+Alaska_Employment08 <- dplyr::filter(Alaska_Employment08, AVERAGE.EMPLOYMENT > 0)
+Alaska_Employment08 <- dplyr::filter(Alaska_Employment08, (NA. %in% c("NATURAL RESOURCES & MINING", "MANUFACTURING", "TRADE, TRANS. & UTILITIES", "LEISURE & HOSPITALITY" )))
+Alaska_Employment08
+
+# 2007 --------------------------------------------------------------------
+Alaska_Employment07 = read_excel("Livelihoods/Employment_Figures/USA/Alaska_Employment_2002_2014.xlsx", sheet = 10, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+Alaska_Employment07<-data.frame(Alaska_Employment07)
+Alaska_Employment07 = dplyr::filter(Alaska_Employment07, (AREA.CODE %in% c("000185", "000188")))
+Alaska_Employment07 <- dplyr::tbl_df(Alaska_Employment07)
+Alaska_Employment07 <- dplyr::select(Alaska_Employment07, AREANAME, NAICS.DESCRIPTION, NA., NA..1, NA..2, AVERAGE.EMPLOYMENT)
+Alaska_Employment07 <- dplyr::filter(Alaska_Employment07, AVERAGE.EMPLOYMENT > 0)
+Alaska_Employment07 <- dplyr::filter(Alaska_Employment07, (NA. %in% c("NATURAL RESOURCES & MINING", "MANUFACTURING", "TRADE, TRANS. & UTILITIES", "LEISURE & HOSPITALITY" )))
+Alaska_Employment07
+
+# 2006 --------------------------------------------------------------------
+Alaska_Employment06 = read_excel("Livelihoods/Employment_Figures/USA/Alaska_Employment_2002_2014.xlsx", sheet = 11, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+Alaska_Employment06<-data.frame(Alaska_Employment06)
+Alaska_Employment06 = dplyr::filter(Alaska_Employment06, (AREA.CODE %in% c("000185", "000188")))
+Alaska_Employment06 <- dplyr::tbl_df(Alaska_Employment06)
+Alaska_Employment06 <- dplyr::select(Alaska_Employment06, AREANAME, NAICS.DESCRIPTION, NA., NA..1, NA..2, AVERAGE.EMPLOYMENT)
+Alaska_Employment06 <- dplyr::filter(Alaska_Employment06, AVERAGE.EMPLOYMENT > 0)
+Alaska_Employment06 <- dplyr::filter(Alaska_Employment06, (NA. %in% c("NATURAL RESOURCES & MINING", "MANUFACTURING", "TRADE, TRANS. & UTILITIES", "LEISURE & HOSPITALITY" )))
+Alaska_Employment06
+
+# 2005 --------------------------------------------------------------------
+Alaska_Employment05 = read_excel("Livelihoods/Employment_Figures/USA/Alaska_Employment_2002_2014.xlsx", sheet = 12, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+Alaska_Employment05<-data.frame(Alaska_Employment05)
+Alaska_Employment05 = dplyr::filter(Alaska_Employment05, (AREA.CODE %in% c("000185", "000188")))
+Alaska_Employment05 <- dplyr::tbl_df(Alaska_Employment05)
+Alaska_Employment05 <- dplyr::select(Alaska_Employment05, AREANAME, NAICS.DESCRIPTION, NA., NA..1, NA..2, AVERAGE.EMPLOYMENT)
+Alaska_Employment05 <- dplyr::filter(Alaska_Employment05, AVERAGE.EMPLOYMENT > 0)
+Alaska_Employment05 <- dplyr::filter(Alaska_Employment05, (NA. %in% c("NATURAL RESOURCE & MINING", "MANUFACTURING", "TRADE, TRANS. & UTILITIES", "LEISURE & HOSPITALITY" )))
+Alaska_Employment05
+##Slight name change in Resource rather than Resources
+
+# 2004 --------------------------------------------------------------------
+Alaska_Employment04 = read_excel("Livelihoods/Employment_Figures/USA/Alaska_Employment_2002_2014.xlsx", sheet = 13, col_names = TRUE, col_types = NULL, na = "", skip = 0)
+Alaska_Employment04<-data.frame(Alaska_Employment04)
+Alaska_Employment04 = dplyr::filter(Alaska_Employment04, (AREA.CODE %in% c("000185", "000188")))
+Alaska_Employment04 <- dplyr::tbl_df(Alaska_Employment04)
+Alaska_Employment04 <- dplyr::select(Alaska_Employment04, AREANAME, NAICS.DESCRIPTION, NA., NA..1, NA..2, AVERAGE.EMPLOYMENT)
+Alaska_Employment04 <- dplyr::filter(Alaska_Employment04, AVERAGE.EMPLOYMENT > 0)
+Alaska_Employment04 <- dplyr::filter(Alaska_Employment04, (NA. %in% c("NATURAL RESOURCE & MINING", "MANUFACTURING", "TRADE, TRANS. & UTILITIES", "LEISURE & HOSPITALITY" )))
+Alaska_Employment04
+
