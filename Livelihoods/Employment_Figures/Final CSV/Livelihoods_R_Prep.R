dcd902fe2f1b73c3ec0437183a5376bac4d73ea7
mburgass <m.burgass14@imperial.ac.uk>
2016-06-10 13:40:47 +0100

    updating to include sectors

--- Livelihoods/Employment_Figures/Final CSV/Livelihoods_R_Prep.R
+++ Livelihoods/Employment_Figures/Final CSV/Livelihoods_R_Prep.R
@@ -3,24 +3,27 @@ library(dplyr)
 Troms_Employment = read_excel("Livelihoods/Employment_Figures/Norway/Troms_Employment_2009_2014.xlsx", sheet = 1, col_names = TRUE, col_types = NULL, na = "", skip = 0)
 Troms_Employment2 = data.frame(Troms_Employment)
 Troms_Fish = Troms_Employment2 %>% filter(Job.Type == "03 Fishing and aquaculture")
+Troms_Fish$Job.Type<-"fishing"
 Troms_Food = Troms_Employment2 %>% filter(Job.Type == "10 Food products")
+Troms_Food$Job.Type<-"food"
 Troms_Water = Troms_Employment2 %>% filter(Job.Type == "50 Water transport")
+Troms_Water$Job.Type<- "transport"
 Troms_Travel = Troms_Employment2 %>% filter(Job.Type == "79 Travel agency, tour operators")
+Troms_Travel$Job.Type<- "tourism"
 
 ## Ocean based employment considering food products likely to be seafood related, travel tourism likely be ocean related
 # Join together
+
+
 Troms_Group = rbind(Troms_Fish, Troms_Food, Troms_Water, Troms_Travel)
-Troms_Group2 = select(Troms_Group, -Job.Type)
-Troms_Group2 = gather(Troms_Group, "Year", "Marine.Jobs", 3:8)
-Troms_Group3 = select(Troms_Group2, -Job.Type)
-Troms_Group3
-Troms_Jobs = Troms_Group3 %>% separate(Year,c("X","Year"),remove=T,sep="X")%>%  #(1) First strip away the X from the years (this creates a new column called "X" that is empty)
-  select(Region,Year,Marine.Jobs)%>%
+Troms_Group<-plyr::rename(Troms_Group, c("Job.Type"="sector"))
+Troms_Group2 = gather(Troms_Group, "Year", "value", 3:8)
+
+Troms_Jobs = Troms_Group2 %>% separate(Year,c("X","Year"),remove=T,sep="X")%>%  #(1) First strip away the X from the years (this creates a new column called "X" that is empty)
+  select(Region,Year,sector, value)%>%
   group_by(Year)%>%
-  dplyr::summarize(total_jobs = sum(Marine.Jobs, na.rm=T))%>%
   ungroup()
-Troms_Jobs$Region <- "Troms"
-troms_jobs_final=Troms_Jobs[c(3,1,2)]
+troms_jobs_final=Troms_Jobs[c(1,3,2,4)]
 troms_jobs_final
 
 # Nordland Jobs -----------------------------------------------------------
@@ -28,70 +31,69 @@ troms_jobs_final
 Nordland_Employment = read_excel("Livelihoods/Employment_Figures/Norway/Nordland_Employment_2009_2014.xlsx", sheet = 1, col_names = TRUE, col_types = NULL, na = "", skip = 0)
 Nord_Employment2 = data.frame(Nordland_Employment)
 Nord_Fish = Nord_Employment2 %>% filter(Job.Type == "03 Fishing and aquaculture")
+Nord_Fish$Job.Type<-"fishing"
 Nord_Food = Nord_Employment2 %>% filter(Job.Type == "10 Food products")
+Nord_Food$Job.Type<-"food"
 Nord_Water = Nord_Employment2 %>% filter(Job.Type == "50 Water transport")
+Nord_Water$Job.Type<- "transport"
 Nord_Travel = Nord_Employment2 %>% filter(Job.Type == "79 Travel agency, tour operators")
-
+Nord_Travel$Job.Type<- "tourism"
 ## Ocean based employment considering food products likely to be seafood related, travel tourism likely be ocean related
 
 # Join together
 Nord_Group = rbind(Nord_Fish, Nord_Food, Nord_Water, Nord_Travel)
-Nord_Group2 = select(Nord_Group, -Job.Type)
-Nord_Group2 = gather(Nord_Group, "Year", "Marine.Jobs", 3:8)
-Nord_Group3 = select(Nord_Group2, -Job.Type)
-Nord_Group3
+Nord_Group<-plyr::rename(Nord_Group, c("Job.Type"="sector"))
+Nord_Group2 = gather(Nord_Group, "Year", "value", 3:8)
 
-Nord_Jobs = Nord_Group3 %>% separate(Year,c("X","Year"),remove=T,sep="X")%>%  #(1) First strip away the X from the years (this creates a new column called "X" that is empty)
-  select(Region,Year,Marine.Jobs)%>%
+Nord_Jobs = Nord_Group2 %>% separate(Year,c("X","Year"),remove=T,sep="X")%>%  #(1) First strip away the X from the years (this creates a new column called "X" that is empty)
+  select(Region,Year,sector, value)%>%
   group_by(Year)%>%
-  dplyr::summarize(total_jobs = sum(Marine.Jobs, na.rm=T))%>%
   ungroup()
-Nord_Jobs$Region <- "Nordland"
-Nord_jobs_final=Nord_Jobs[c(3,1,2)]
-Nord_jobs_final
+nord_jobs_final=Nord_Jobs[c(1,3,2,4)]
+nord_jobs_final$Region<-"Nordland"
 
 # Finnmark Jobs -----------------------------------------------------------
 
 Finnmark_Employment = read_excel("Livelihoods/Employment_Figures/Norway/Finnmark_Employment_2009_2014.xlsx", sheet = 1, col_names = TRUE, col_types = NULL, na = "", skip = 0)
 Finn_Employment2 = data.frame(Finnmark_Employment)
 Finn_Fish = Finn_Employment2 %>% filter(Job.Type == "03 Fishing and aquaculture")
+Finn_Fish$Job.Type<-"fishing"
 Finn_Food = Finn_Employment2 %>% filter(Job.Type == "10 Food products")
+Finn_Food$Job.Type<-"food"
 Finn_Water = Finn_Employment2 %>% filter(Job.Type == "50 Water transport")
+Finn_Water$Job.Type<- "transport"
 Finn_Travel = Finn_Employment2 %>% filter(Job.Type == "79 Travel agency, tour operators")
+Finn_Travel$Job.Type<- "tourism"
 
 ## Ocean based employment considering food products likely to be seafood related, travel tourism likely be ocean related
 
 # Join together
 Finn_Group = rbind(Finn_Fish, Finn_Food, Finn_Water, Finn_Travel)
-Finn_Group2 = select(Finn_Group, -Job.Type)
-Finn_Group2 = gather(Finn_Group, "Year", "Marine.Jobs", 3:8)
-Finn_Group3 = select(Finn_Group2, -Job.Type)
-Finn_Group3
+Finn_Group<-plyr::rename(Finn_Group, c("Job.Type"="sector"))
+Finn_Group2 = gather(Finn_Group, "Year", "value", 3:8)
 
-Finn_Jobs = Finn_Group3 %>% separate(Year,c("X","Year"),remove=T,sep="X")%>%  #(1) First strip away the X from the years (this creates a new column called "X" that is empty)
-  select(Region,Year,Marine.Jobs)%>%
+Finn_Jobs = Finn_Group2 %>% separate(Year,c("X","Year"),remove=T,sep="X")%>%  #(1) First strip away the X from the years (this creates a new column called "X" that is empty)
+  select(Region,Year,sector, value)%>%
   group_by(Year)%>%
-  dplyr::summarize(total_jobs = sum(Marine.Jobs, na.rm=T))%>%
   ungroup()
-Finn_Jobs$Region <- "Finnmark"
-Finn_jobs_final=Finn_Jobs[c(3,1,2)]
-Finn_jobs_final
+Finn_jobs_final=Finn_Jobs[c(1,3,2,4)]
+Finn_jobs_final$Region<-"Finnmark"
 
 # Norway Mainland Jobs ----------------------------------------------------
 
-norway_main_jobs = rbind(troms_jobs_final, Nord_jobs_final, Finn_jobs_final)%>%
-  group_by(Year)%>%
-  dplyr::summarize(Marine.Jobs = sum(total_jobs, na.rm=T))%>%
+norway_main_jobs = rbind(troms_jobs_final, nord_jobs_final, Finn_jobs_final)%>%
+  group_by(Year, sector)%>%
+  dplyr::summarize(value = sum(value, na.rm=T))%>%
   ungroup()
-norway_main_jobs$Region <- "Norway Mainland"
-norway_mainland_jobs=norway_main_jobs[c(3,1,2)]
+norway_main_jobs$rgn_id <- "Norway Mainland"
+norway_mainland_jobs=norway_main_jobs[c(4,2,1,3)]
 norway_mainland_jobs
 
 # Svalbard ----------------------------------------------------------------
 Svalbard_Employment = read_excel("Livelihoods/Employment_Figures/Norway/Svalbard_Employment_2008_2014.xlsx", sheet = 1, col_names = TRUE, col_types = NULL, na = "", skip = 0)
 Sval_Employment2 = data.frame(Svalbard_Employment)%>%
-  rename(c("Sector"="Job.Type"))
-Sval_Employment2<-filter(Sval_Employment2, !(Job.Type %in% c('B Mining and quarrying', 'C-D-E Manufacturing', 'F Construction',
+  rename(c("Sector"="sector"))
+Sval_Employment2<-filter(Sval_Employment2, !(sector %in% c('B Mining and quarrying', 'C-D-E Manufacturing', 'F Construction',
                                                              'G Wholesale and retail trade: repair of motor vehicles and motorcycles',
                                                              'J-K Information and communication', 'L Real estate activities',
                                                              'M Professional, scientific and technical activities', 'N Administrative and support service activities',
@@ -101,81 +103,83 @@ Sval_Employment2<-filter(Sval_Employment2, !(Job.Type %in% c('B Mining and quarr
 
 # Join together
 
-Sval_Employment2<-gather(Sval_Employment2, "Year", "Marine.Jobs", 2:8)
-Sval_Employment2<-select(Sval_Employment2, -Job.Type)
-Sval_Employment2
+Sval_Employment2<-gather(Sval_Employment2, "Year", "value", 2:8)
+
 
 Sval_Jobs = Sval_Employment2 %>% separate(Year,c("X","Year"),remove=T,sep="X")%>%  #(1) First strip away the X from the years (this creates a new column called "X" that is empty)
-  select(Year,Marine.Jobs)%>%
-  group_by(Year)%>%
-  dplyr::summarize(Marine.Jobs = sum(Marine.Jobs, na.rm=T))%>%
-  ungroup()
-Sval_Jobs$Region <- "Svalbard"
-sval_jobs_final=Sval_Jobs[c(3,1,2)]
-sval_jobs_final
+  select(Year,sector,value)
+  
+Sval_Jobs$rgn_id <- "Svalbard"
+sval_jobs_final=Sval_Jobs[c(4,2,1,3)]
+sval_jobs_final$sector<-as.character(sval_jobs_final$sector) ## change to character
+sval_jobs_final[sval_jobs_final=="P Education"]<- "education"
+sval_jobs_final[sval_jobs_final=="H Transportation and storage"]<-"transport"
+sval_jobs_final[sval_jobs_final=="I Accommodation and food service activities"]<-"hospitality"
+sval_jobs_final[sval_jobs_final=="R Arts, entertainment and recreation"]<-"tourism"
 
 # Norway Jobs bind --------------------------------------------------------
 
 
 norway_jobs = rbind(sval_jobs_final, norway_mainland_jobs)
-norway_jobs$Marine.Jobs<- round(norway_jobs$Marine.Jobs, digits=0)
+norway_jobs$value<- round(norway_jobs$value, digits=0)
 norway_jobs
-write.csv(norway_jobs, "Norway_Jobs.csv")
+
 
 # Canada ------------------------------------------------------------------
 
 
 # NWT ---------------------------------------------------------------------
 NWT_Employment = read.csv('Livelihoods/Employment_Figures/Canada/NWT_Employment_2001_2015.csv')
-NWT_Employment2 = data.frame(NWT_Employment)
-NWT_Employment2<-dplyr::filter(NWT_Employment2, (Job.Type %in% c("Transportation and warehousing", "Accommodation and food services", "Information, culture and recreation")))
+NWT_Employment2 = data.frame(NWT_Employment)%>%
+  rename(c("Job.Type"="sector"))
+NWT_Employment2<-dplyr::filter(NWT_Employment2, (sector %in% c("Transportation and warehousing", "Accommodation and food services", "Information, culture and recreation")))
 ## Assume related to ship transport, tourism, ship business
 
-NWT_Employment2<-gather(NWT_Employment2, "Year", "Marine.Jobs", 2:16)
-NWT_Employment2<-select(NWT_Employment2, -Job.Type)
+NWT_Employment2<-gather(NWT_Employment2, "Year", "value", 2:16)
+
 NWT_Employment2
 
 NWT_Jobs = NWT_Employment2 %>% separate(Year,c("X","Year"),remove=T,sep="X")%>%  #(1) First strip away the X from the years (this creates a new column called "X" that is empty)
-  select(Year,Marine.Jobs)%>%
-  group_by(Year)%>%
-  dplyr::summarize(Marine.Jobs = sum(Marine.Jobs, na.rm=T))%>%
-  ungroup()
+  select(Year,sector,value)
 NWT_Jobs$Region <- "Beaufort"
-NWT_jobs_final=NWT_Jobs[c(3,1,2)]
-NWT_jobs_final
+NWT_jobs_final=NWT_Jobs[c(4,2,1,3)]
+NWT_jobs_final$sector<-as.character(NWT_jobs_final$sector) 
+NWT_jobs_final[NWT_jobs_final=="Transportation and warehousing"]<- "transport"
+NWT_jobs_final[NWT_jobs_final=="Information, culture and recreation"]<-"tourism"
+NWT_jobs_final[NWT_jobs_final=="Accommodation and food services"]<-"hospitality"
 
 ##Beaufort Delta area accounts for ~14.2% of NWT employment based on data from
 ##C:\Users\MB4514\Documents\github\pre-proc\Livelihoods\Employment_Figures\Raw files\Canada\Community Labour Force Activity, 1986 to 2014.xlsx
 ##Now calculate Beaufort Delta region based on 14.2% of NWT_jobs_final
 
-NWT_jobs_final=dplyr::mutate(NWT_jobs_final, Marine.Jobs = (NWT_jobs_final$Marine.Jobs/100)*14.2)
-NWT_jobs_final$Marine.Jobs<- round(NWT_jobs_final$Marine.Jobs, digits=0)
+NWT_jobs_final=dplyr::mutate(NWT_jobs_final, value = (NWT_jobs_final$value/100)*14.2)
+NWT_jobs_final$value<- round(NWT_jobs_final$value, digits=0)
 
 # Nunavut -----------------------------------------------------------------
 
 Nunavut_Employment = read_excel("Livelihoods/Employment_Figures/Canada/Nunavut_Employment_2008_2014.xlsx", sheet = 1, col_names = TRUE, col_types = NULL, na = "", skip = 0)
-Nunavut_Employment<-data.frame(Nunavut_Employment)
-Nunavut_Employment2 = dplyr::filter(Nunavut_Employment, (Job.Type %in% c("Fishing, Hunting, Trapping, Mining and Quarrying", "Transportation and Warehousing", "Accommodation and Food Services")))
+Nunavut_Employment<-data.frame(Nunavut_Employment)%>%  rename(c("Job.Type"="sector"))
+
+Nunavut_Employment2 = dplyr::filter(Nunavut_Employment, (sector %in% c("Fishing, Hunting, Trapping, Mining and Quarrying", "Transportation and Warehousing", "Accommodation and Food Services")))
 ## Job types quite general - taken ones with most likely marine connections.
 
-Nunavut_Employment2<-gather(Nunavut_Employment2, "Year", "Marine.Jobs", 2:8)
-Nunavut_Employment2<-select(Nunavut_Employment2, -Job.Type)
+Nunavut_Employment2<-gather(Nunavut_Employment2, "Year", "value", 2:8)
+
 Nunavut_Jobs = Nunavut_Employment2 %>% separate(Year,c("X","Year"),remove=T,sep="X")%>%
-  select(Year, Marine.Jobs)%>%
-  group_by(Year)%>%
-  dplyr::summarize(Marine.Jobs = sum(Marine.Jobs, na.rm=T))%>%
-  ungroup()
-Nunavut_Jobs
+  select(Year, sector, value)
 Nunavut_Jobs$Region <- "Nunavut"
-Nunavut_jobs_final=Nunavut_Jobs[c(3,1,2)]
-Nunavut_jobs_final
+Nunavut_jobs_final=Nunavut_Jobs[c(4,2,1,3)]
+Nunavut_jobs_final$sector<-as.character(Nunavut_jobs_final$sector) 
+Nunavut_jobs_final[Nunavut_jobs_final=="Transportation and Warehousing"]<- "transport"
+Nunavut_jobs_final[Nunavut_jobs_final=="Fishing, Hunting, Trapping, Mining and Quarrying"]<-"fishing"
+Nunavut_jobs_final[Nunavut_jobs_final=="Accommodation and Food Services"]<-"hospitality"
 
 
 # Canada Join -------------------------------------------------------------
 
 canada_jobs = rbind(NWT_jobs_final, Nunavut_jobs_final)
 canada_jobs
-write.csv(canada_jobs, "Canada_Jobs.csv")
+
 
 # Greenland ---------------------------------------------------------------
 
@@ -195,7 +199,7 @@ Greenland_jobs_final
 
 ##Check how best to separate out between east and west Greenland? Roughly 80% of people live in West Greenland
 
-write.csv(Greenland_jobs_final, "Greenland_Jobs.csv")
+
 
 # USA/Alaska --------------------------------------------------------------
 
@@ -340,7 +344,7 @@ Alaska_Jobs = Alaska_Group%>% group_by(Year)%>%
 Alaska_Jobs$Region <- "Alaska"
 Alaska_Jobs<-Alaska_Jobs[c(3,1,2)]
 Alaska_Jobs
-write.csv(Alaska_Jobs, "Alaska_Jobs.csv")
+
 
 # Russia ------------------------------------------------------------------
 
@@ -449,4 +453,13 @@ Russia_Group_Wide$Sakha<- round(Russia_Group_Wide$Sakha, digits=0)
 
 Russia_Jobs<- gather(Russia_Group_Wide, District, Marine.Jobs, 2:7)
 Russia_Jobs<-Russia_Jobs[c(2,1,3)]
-write.csv(Russia_Jobs, "Russia_Jobs.csv")
+Russia_Jobs<-Russia_Jobs%>% group_by(Year)%>%
+  dplyr::summarize(Marine.Jobs = sum(Marine.Jobs, na.rm=T))%>%
+  ungroup()
+Russia_Jobs$Region<-"Russia"
+Russia_Jobs<-Russia_Jobs[c(3,1,2)]
+Russia_Jobs
+
+le_jobs_year= rbind(Russia_Jobs, Alaska_Jobs, canada_jobs, norway_jobs, Greenland_jobs_final)
+le_jobs_year
+write.csv(le_jobs_year, "le_jobs_year.csv")
